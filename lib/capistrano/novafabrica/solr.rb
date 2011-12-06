da09c6306eecf48900a4fc579c3149d19cb0125a
Kevin Skoglund <kevin@novafabrica.com>
2011-12-06 13:51:19 -0500

    bugfix: solr:udpate was still being called

--- lib/capistrano/novafabrica/solr.rb
+++ lib/capistrano/novafabrica/solr.rb
@@ -6,15 +6,13 @@ configuration = Capistrano::Configuration.respond_to?(:instance) ?
 
 configuration.load do
   
-  after "deploy:update_code", "solr:update"  
-  
   namespace :solr do
 
-  desc "Solr Reindex"
-  task :reindex, :role => :web do
-    run "cd #{current_path}; bundle exec rake RAILS_ENV=#{rails_env} maharam:reindex"
-  end
+    desc "Solr Reindex"
+    task :reindex, :role => :web do
+      run "cd #{current_path}; bundle exec rake RAILS_ENV=#{rails_env} maharam:reindex"
+    end
   
-end
+  end
   
 end
