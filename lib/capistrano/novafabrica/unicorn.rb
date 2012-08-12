81629dbbaf5be54848f0b69bcc7a4a4563f1ca98
Matthew Bergman <mzbphoto@gmail.com>
2012-08-12 00:17:45 -0400

    added unicorn to gem

---
+++ lib/capistrano/novafabrica/unicorn.rb
@@ -0,0 +1,38 @@
+require 'capistrano/novafabrica/helper'
+
+configuration = Capistrano::Configuration.respond_to?(:instance) ?
+  Capistrano::Configuration.instance(:must_exist) :
+  Capistrano.configuration(:must_exist)
+
+configuration.load do
+  
+  after "deploy:update_code", "unicorn:restart"
+  
+  _cset :unicorn_ctrl, "/etc/init.d/unicorn"
+
+  namespace :unicorn do
+
+    desc "start unicorn"
+    task :start do
+      "#{unicorn_ctrl} start"
+    end
+
+    desc "stop unicorn"
+    task :stop do
+      "#{unicorn_ctrl} stop"
+    end
+
+    desc "stop and start unicorn"
+    task :restart do
+      tomcat.stop
+      tomcat.start
+    end
+
+    desc "tail :current_path/log/unicorn.stderr.log"
+    task :tail do
+      stream "tail -f #{current_path}/log/unicorn.stderr.log"
+    end
+
+  end
+
+end
